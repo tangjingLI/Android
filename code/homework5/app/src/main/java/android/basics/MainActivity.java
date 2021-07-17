6b08ea939bc798170ab47b3e2092b4fb099f6c41
tangjingLI <942855221@qq.com>
2021-07-17 12:41:17 +0800

    h5

---
+++ code/homework5/app/src/main/java/android/basics/MainActivity.java
@@ -0,0 +1,17 @@
+package android.basics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("daqi", "current branch HEAD: " + BuildConfig.commitHash);
+    }
+}
